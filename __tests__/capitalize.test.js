05bc769d8b52718ea3c2934f96b79a4575d6ab35
da.lasitsa <neolakalasua641@gmail.com>
2024-02-16 19:48:09 +0200

    ADD: __tests__/, index.test.js, index.js, package-lock.json UPDATE: package.json

--- __tests__/capitalize.test.js
+++ __tests__/capitalize.test.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env node
-
 import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
 assert.equal(capitalize('hello'), 'Hello');
 assert.equal(capitalize(''), '');
-
+1
 console.log('Все тесты пройдены!');
